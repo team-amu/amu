c21945829564750f293b62f73cc16d5b5bff6b2d
imukyee <imukyee@gmail.com>
2022-05-20 14:10:08 +0900

    Revice: Rename Django Project name

--- final-pjt-back/amu/asgi.py
+++ final-pjt-back/amu/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for makemodel project.
+ASGI config for amu project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'makemodel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amu.settings')
 
 application = get_asgi_application()
