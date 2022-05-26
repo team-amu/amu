b5e110b47a73acf852a9acec2e1e402a8e2ebfbc
DESKTOP-92SVKLH\tmdgn <tmdgns1126@naver.com>
2022-05-27 01:27:01 +0900

    Add: 404 페이지 간단한 구현

--- final-pjt-front/src/views/NotFound404.vue
+++ final-pjt-front/src/views/NotFound404.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <h1>404 Not Found</h1>
+  <div class="container-404">
+    <h1 class="main-title">404 Not Found</h1>
+    <h1 class="main-title">잘못된 요청입니다!</h1>
+    <button id="back-button" @click="goBack">이전으로 돌아가기</button>
+    
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: "NotFound404",
+  methods: {
+    ...mapActions(['goBack'])
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.container-404 {
+  @include flex(column);
 
+  .main-title {
+    @include main-title;
+  }
+
+  #back-button {
+    @include back-btn1;
+  }
+}
 </style>
