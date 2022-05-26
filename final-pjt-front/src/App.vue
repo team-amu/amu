bd1924333bf4f8660eb7bc3a9ed9ff2ca7ba2d05
DESKTOP-92SVKLH\tmdgn <tmdgns1126@naver.com>
2022-05-26 15:27:38 +0900

    Add: nav drawer 디자인

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -2,37 +2,65 @@
   <v-app id="app">
     <v-navigation-drawer
       v-model="drawer"
+      id="navigation-drawer"
+      color='#212121'
       app
     >
-      <!-- 계정 관련 -->
-      <section>
-        <router-link :to="{ name: 'login' }">로그인</router-link> <br>
-        <router-link :to="{ name: 'logout'}">로그아웃</router-link> <br>
-        <router-link :to="{ name: 'signup'}">회원가입</router-link>
+      <section id="nav-account">
+        <router-link
+          id="user-profile-image-container"
+          :to="{ name: 'profileLike', params: { username: currentUser.username }}"
+        >
+          <div
+            id="user-profile-image"
+            :style="profileImageSrc"
+          ></div>
+        </router-link>
+        
+
+        <div id="user-nickname">
+          {{ nickname }}
+        </div>
+
+        <router-link
+          v-if="isLoggedIn"
+          :to="{ name: 'logout'}"
+          class="account-link"
+        >로그아웃</router-link>
+
+        <router-link 
+          v-if="!isLoggedIn"
+          class="account-link"
+          :to="{ name: 'login' }"
+        >로그인</router-link>
+
       </section>
-      <hr>
+
       <!-- profile 관련 -->
-      <section v-if="isCurrentUser">
-        <router-link :to="{ name: 'profileLike', params: { username: currentUser.username }}">좋아요한 영화</router-link> <br>
-        <router-link :to="{ name: 'profileBookmark',  params: { username: currentUser.username }}">북마크한 영화</router-link> <br>
-        <router-link :to="{ name: 'profileArticle',  params: { username: currentUser.username }}">내가 쓴 게시글</router-link> <br>
-        <router-link :to="{ name: 'profileComment',  params: { username: currentUser.username }}">내가 쓴 댓글</router-link>
+      <section id="nav-profile" v-if="isCurrentUser">
+        <div class="section-title">MY PAGE</div>
+        <router-link :to="{ name: 'profileLike', params: { username: currentUser.username }}">나의 좋아요 영화</router-link>
+        <router-link :to="{ name: 'profileBookmark',  params: { username: currentUser.username }}">나의 북마크 영화</router-link>
+        <router-link :to="{ name: 'profileArticle',  params: { username: currentUser.username }}">나의 게시글</router-link>
+        <router-link :to="{ name: 'profileComment',  params: { username: currentUser.username }}">나의 댓글</router-link>
       </section>
-      <hr>
-      <section>
+
+      <section id="nav-movie">
+        <div class="section-title">MOVIE</div>
         <!-- movie 관련 -->
-        <router-link :to="{ name: 'home' }">홈 화면</router-link> <br>
+        <router-link :to="{ name: 'home' }">HOME</router-link>
         <!-- params 없이 들어갔을 때 막아주기 -->
         <router-link :to="{ name: 'movieSearch', params: { searchPage: '1'}, query: {searchKeywords: '', 
-      type: 'title', genres: [], minRank: 0, sort: '-release_date'}}">영화 검색 페이지</router-link>
+      type: 'title', genres: [], minRank: 0, sort: '-release_date'}}">SEARCH</router-link>
       </section>
-      <hr>
-      <section>
+
+      <section id="nav-community">
         <!-- community 관련 -->
-        <router-link :to="{ name: 'articleCreate' }">게시글 쓰기</router-link> <br>
-        <router-link :to="{ name: 'communityTotal', params: {page:'1'} }">전체게시판</router-link> <br>
-        <router-link :to="{ name: 'communityHot', params: {page:'1'} }">인기게시판</router-link> <br>
-        <router-link :to="{ name: 'communityReview', params: {page:'1'} }">영화게시판</router-link> <br>
+        <div class="section-title">COMMUNITY</div>
+        <router-link :to="{ name: 'articleCreate' }">게시글 작성</router-link>
+        <router-link :to="{ name: 'communityTotal', params: {page:'1'} }">전체게시판</router-link>
+        <router-link :to="{ name: 'communityHot', params: {page:'1'} }">인기게시판</router-link>
+        <router-link :to="{ name: 'communityReview', params: {page:'1'} }">영화게시판</router-link>
         <router-link :to="{ name: 'communityFree', params: {page:'1'} }">자유게시판</router-link>
       </section>
     </v-navigation-drawer>
@@ -41,8 +69,12 @@
       app
       color='#212121'
     >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Archive Of Movie</v-toolbar-title>
+      <v-app-bar-nav-icon color='white' @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link id="nav-main-title" :to="{ name: 'home' }">
+          Archive Of Movie
+        </router-link>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -53,16 +85,7 @@
   </v-app>
 </template>
 
- <script>
-// import HomeView from '@/components/profile/ProfileUserSection.vue'
-// export default {
-//   components: { HomeView },
-//   name: "App",
-//   computed: {
-//   },
-//   methods: {
-//   }
-// }
+<script>
   import { mapActions, mapGetters } from 'vuex'
 
   export default {
@@ -73,10 +96,20 @@
       ...mapActions(['fetchCurrentUser', 'fetchMyProfile']),
     },
     computed: {
-      ...mapGetters(['currentUser', 'isCurrentUser']),
+      ...mapGetters(['currentUser', 'currentUserProfile', 'isCurrentUser', 'isLoggedIn']),
       username() {
         return this.currentUser.username? this.currentUser.username : 'guest'
       },
+      profileImageSrc() {
+        const profile_image = this.currentUserProfile.profile_image
+        return {
+          'background-image': `url("http://localhost:8000${profile_image}")`,
+          'background-size': 'cover'
+        }
+      },
+      nickname() {
+        return this.currentUserProfile.nickname
+      }
     },
     created() {
       this.fetchCurrentUser()
@@ -88,4 +121,82 @@
 
 <style lang="scss">
 @import '@/styles/_app.scss';
+@include scrollbar;
+@mixin nav-section {
+  @include flex-gap(column, .5);
+  align-items: center;
+  border-bottom: 1px solid white;
+  margin: 2vw 1em;
+  padding: 0 0 2vw 0;
+  flex-basis: 100%;
+  flex-shrink: 1;
+}
+
+#navigation-drawer {
+  a {
+    text-decoration: none;
+    color: white;
+    @include hover;
+  }
+
+  .section-title {
+    @include f-4;
+    font-weight: 700;
+    color: mix(white, $dm-pt-color1, .8);
+  }
+
+  @include flex-gap(column, .5);
+  #nav-account {
+    @include wh-ratio(1, 1);
+    @include nav-section;
+
+    #user-profile-image-container {
+      @include flex;
+      width: 100%;
+
+      #user-profile-image {
+        @include wh-ratio(1, 1);
+        flex-basis: 20em;
+        border-radius: 50%;
+        border: 1px solid white;
+        margin: 1em;
+      }
+    }
+
+    #user-nickname {
+      @include f-4;
+      font-weight: 800;
+    }
+
+    .account-link {
+      @include f-6;
+      color: lightgrey;
+    }
+  }
+
+  #nav-profile {
+    @include nav-section;
+  }
+
+  #nav-movie {
+    @include nav-section;
+  }
+
+  #nav-community {
+    @include nav-section;
+  }
+}
+
+#nav-main-title {
+  text-decoration: none;
+  font-weight: 800;
+
+  &:link {
+    color: white;
+  }
+
+  &:visited {
+    color: white;
+  }
+}
 </style>
